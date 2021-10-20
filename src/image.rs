ead4b60dc2ffa7562190070e2d2750ca8ab98eeb
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2021-10-20 20:34:22 +0200

    Add documentation comments to existing code

--- src/image.rs
+++ src/image.rs
@@ -1,6 +1,8 @@
 use std::ops::{Index, IndexMut};
 use std::slice::{ChunksExact, ChunksExactMut};
 
+/// Pixels are represented using three floating-point color channels,
+/// with range from `0.0` to `1.0`. There is no alpha channel.
 #[derive(Debug, Clone, Copy)]
 pub struct Pixel {
     pub r: f32,
@@ -9,6 +11,7 @@ pub struct Pixel {
 }
 
 impl Default for Pixel {
+    /// Default-constructs a completely black pixel.
     fn default() -> Pixel {
         Pixel {
             r: 0.0,
@@ -18,6 +21,8 @@ impl Default for Pixel {
     }
 }
 
+/// An image is a two-dimensional matrix of pixels, with its origin
+/// in the top left corner.
 #[derive(Debug)]
 pub struct Image {
     width: usize,
@@ -26,6 +31,21 @@ pub struct Image {
 }
 
 impl Image {
+    /// Contstructs an image with a given size.
+    ///
+    /// # Arguments
+    ///
+    /// * `width` - the width of the image (number of columns)
+    /// * `height` - the height of the image (number of rows)
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use raytrust::image::Image;
+    /// let image = Image::new(32, 32);
+    /// assert_eq!(image.width(), 32);
+    /// assert_eq!(image.height(), 32);
+    /// ```
     pub fn new(width: usize, height: usize) -> Image {
         let pixels = vec![Pixel::default(); width * height].into_boxed_slice();
         Image {
@@ -35,18 +55,41 @@ impl Image {
         }
     }
 
+    /// Returns the width of the image.
     pub fn width(&self) -> usize {
         self.width
     }
 
+    /// Returns the height of the image.
     pub fn height(&self) -> usize {
         self.height
     }
 
+    /// Returns an iterator over rows of the image.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use raytrust::image::Image;
+    /// let image = Image::new(32, 32);
+    /// assert_eq!(image.iter().count(), image.height());
+    /// ```
     pub fn iter(&self) -> ChunksExact<'_, Pixel> {
         self.pixels.chunks_exact(self.width)
     }
 
+    /// Returns an iterator that allows modifying each row.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use raytrust::image::{Image, Pixel};
+    /// let mut image = Image::new(4, 4);
+    /// for row in image.iter_mut() {
+    ///     row[1] = Pixel { r: 0.5, g: 0.5, b: 0.5 };
+    /// }
+    /// assert_eq!(image[0][1].r, 0.5);
+    /// ```
     pub fn iter_mut(&mut self) -> ChunksExactMut<'_, Pixel> {
         self.pixels.chunks_exact_mut(self.width)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,20 @@ use std::io;
 
 pub mod image;
 
+/// Serialize an image using the PGM format.
+///
+/// # Arguments
+///
+/// * `stream` - writer/sink to serialize image into
+/// * `image` - image to serialize
+///
+/// # Example
+///
+/// ```
+/// use raytrust::{image, write_pgm};
+/// let image = image::Image::new(8, 8);
+/// write_pgm(&mut std::io::stdout(), &image);
+/// ```
 pub fn write_pgm(stream: &mut (dyn io::Write), image: &image::Image) -> Result<(), io::Error> {
     writeln!(stream, "P3")?;
     writeln!(stream, "{} {}", image.width(), image.height())?;
