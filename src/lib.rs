cac5d62d948cc7850ae883893615664797bd6f25
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2021-10-20 22:27:56 +0200

    Add unit tests for write_pgm (and fix issues)

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ pub mod image;
 pub fn write_pgm(stream: &mut (dyn io::Write), image: &image::Image) -> Result<(), io::Error> {
     writeln!(stream, "P3")?;
     writeln!(stream, "{} {}", image.width(), image.height())?;
+    writeln!(stream, "255")?;
     for row in image {
         for pixel in row {
             writeln!(
@@ -31,4 +32,39 @@ pub fn write_pgm(stream: &mut (dyn io::Write), image: &image::Image) -> Result<(
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_write_pgm() -> Result<(), io::Error> {
+        let mut image = image::Image::new(1, 2);
+        image[0][0] = image::Pixel {
+            r: 1.0,
+            g: 0.5,
+            b: 0.0,
+        };
+        image[1][0] = image::Pixel {
+            r: 1.25,
+            g: -1.25,
+            b: 0.0,
+        };
+
+        let mut vec: Vec<u8> = Vec::new();
+        write_pgm(&mut vec, &image)?;
+
+        let expected = indoc::indoc! {"
+            P3
+            1 2
+            255
+            255 128 0
+            255 0 0
+        "};
+
+        assert_eq!(expected, std::str::from_utf8(&vec).unwrap());
+        Ok(())
+    }
 }
