0073ab802c2fdea493362bb36307608d5af6db19
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2021-10-25 21:34:33 +0200

    Initial rendering function

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,69 @@ mod image;
 mod types;
 
 pub use image::Image;
+use types::{Point3, Ray, Vect3};
+
+/// Render the color for a specific pixel.
+fn render_ray(ray: Ray) -> image::Pixel {
+    let t = 0.5 * (ray.direction().y + 1.0);
+    image::Pixel {
+        r: (1.0 - t) * 1.0 + t * 0.5,
+        g: (1.0 - t) * 1.0 + t * 0.7,
+        b: (1.0 - t) * 1.0 + t * 1.0,
+    }
+}
+
+/// Render an image by raytracing.
+///
+/// # Arguments
+///
+/// * `width` - output image width
+/// * `height` - output image height
+pub fn render(width: usize, height: usize) -> Image {
+    let mut image = Image::new(width, height);
+
+    // Viewport & focal length
+    let viewport_height = 2.0;
+    let viewport_width = (width as f32 / height as f32) * viewport_height;
+    let focal_length = 1.0;
+
+    // Camera position, horizontal & vertical viewport extents
+    let origin = Point3::zero();
+    let horiz = Vect3 {
+        x: viewport_width,
+        ..Vect3::zero()
+    };
+    let vert = Vect3 {
+        y: viewport_height,
+        ..Vect3::zero()
+    };
+
+    // Lower left corner of the camera view
+    let lower_left_corner = origin
+        - (horiz / 2.0)
+        - (vert / 2.0)
+        - Vect3 {
+            z: focal_length,
+            ..Vect3::zero()
+        };
+
+    // Render the image!
+    let pb = indicatif::ProgressBar::new_spinner();
+    for (y, row) in image.iter_mut().rev().enumerate() {
+        pb.set_message(format!("Rendering line {}/{}", (y + 1), height));
+        for (x, pixel) in row.iter_mut().enumerate() {
+            let u = (x as f32) / ((width as f32) - 1.0);
+            let v = (y as f32) / ((height as f32) - 1.0);
+            let dir = lower_left_corner + (u * horiz) + (v * vert) - origin;
+            let ray = Ray::new(origin, dir);
+            *pixel = render_ray(ray);
+        }
+        pb.tick();
+    }
+    pb.finish();
+
+    image
+}
 
 /// Serialize an image using the PGM format.
 ///
@@ -20,6 +83,7 @@ pub use image::Image;
 /// write_pgm(&mut std::io::stdout(), &image);
 /// ```
 pub fn write_pgm(stream: &mut (dyn io::Write), image: &Image) -> Result<(), io::Error> {
+    let pb = indicatif::ProgressBar::new_spinner().with_message("Saving image");
     writeln!(stream, "P3")?;
     writeln!(stream, "{} {}", image.width(), image.height())?;
     writeln!(stream, "255")?;
@@ -33,7 +97,9 @@ pub fn write_pgm(stream: &mut (dyn io::Write), image: &Image) -> Result<(), io::
                 ((pixel.b * 255.0).round() as u8)
             )?;
         }
+        pb.tick();
     }
+    pb.finish();
     Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::*;
 use core::result::Result;
-use raytrust::{write_pgm, Image};
+use raytrust::{render, write_pgm};
 use std::{fs, io};
 
 fn main() -> Result<(), io::Error> {
@@ -14,11 +14,17 @@ fn main() -> Result<(), io::Error> {
 
     // Argument: output file (or stdout if "-")
     let mut output: Box<dyn io::Write> = match matches.value_of("output") {
-        Some(file) => Box::new(fs::File::open(file).unwrap()),
+        Some(file) => Box::new(
+            fs::OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(file)
+                .unwrap(),
+        ),
         None => Box::new(io::stdout()),
     };
 
     // Sample image
-    let image = Image::new(256, 256);
+    let image = render(800, 450);
     write_pgm(&mut *output, &image)
 }
