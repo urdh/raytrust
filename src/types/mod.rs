46ef5914a5926f2b2b7e51b68d70ea392870b213
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2021-10-25 19:49:44 +0200

    Split Vect3 and Point3 into separate modules

---
+++ src/types/mod.rs
@@ -0,0 +1,6 @@
+/// Useful types for use in a raytracer.
+mod point;
+mod vect;
+
+pub(crate) use point::Point3;
+pub(crate) use vect::Vect3;
--- src/types/vect.rs
+++ src/types/vect.rs
@@ -3,7 +3,7 @@ use std::{f32, fmt};
 
 /// A vector in ℝ³.
 #[derive(Debug, Default, Clone, Copy, PartialEq)]
-struct Vect3 {
+pub struct Vect3 {
     pub x: f32,
     pub y: f32,
     pub z: f32,
@@ -131,71 +131,6 @@ impl DivAssign<f32> for Vect3 {
     }
 }
 
-/// A point in ℝ³.
-#[derive(Debug, Default, Clone, Copy, PartialEq)]
-struct Point3 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
-}
-
-impl Point3 {
-    /// Return the origin point.
-    pub fn zero() -> Point3 {
-        Point3::default()
-    }
-}
-
-impl fmt::Display for Point3 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{{}, {}, {}}}", self.x, self.y, self.z)
-    }
-}
-
-impl Add<Vect3> for Point3 {
-    type Output = Self;
-
-    fn add(self, other: Vect3) -> Self::Output {
-        Point3 {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
-        }
-    }
-}
-
-impl AddAssign<Vect3> for Point3 {
-    fn add_assign(&mut self, other: Vect3) {
-        *self = *self + other
-    }
-}
-
-impl Sub<Vect3> for Point3 {
-    type Output = Self;
-
-    fn sub(self, other: Vect3) -> Self::Output {
-        self + (-other)
-    }
-}
-
-impl SubAssign<Vect3> for Point3 {
-    fn sub_assign(&mut self, other: Vect3) {
-        *self = *self - other
-    }
-}
-
-impl Sub for Point3 {
-    type Output = Vect3;
-
-    fn sub(self, other: Point3) -> Self::Output {
-        Vect3 {
-            x: self.x - other.x,
-            y: self.y - other.y,
-            z: self.z - other.z,
-        }
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -295,41 +230,5 @@ mod test {
         assert_eq!(vect2.cross(vect1), -vect3);
         assert_eq!(vect3.cross(vect2), -vect1);
         assert_eq!(vect1.cross(vect3), -vect2);
-    }
-
-    #[test]
-    fn test_point3_ops() {
-        let zero = Point3::zero();
-        let point = Point3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        };
-        let vect = Vect3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        };
-
-        assert_eq!(zero - point, -vect);
-        assert_eq!(point - zero, vect);
-        assert_eq!(zero + vect, point);
-        assert_eq!(point - vect, zero);
-    }
-
-    #[test]
-    fn test_point3_assign_ops() {
-        let orig = Point3::zero();
-        let mut point = orig;
-        let vect = Vect3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
-        };
-
-        point += vect;
-        assert_eq!(point, orig + vect);
-        point -= vect;
-        assert_eq!(point, orig);
     }
 }
