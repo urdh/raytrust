2b40081e749c7daabd3692acc800e25a018a2937
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2021-10-27 22:22:08 +0200

    Add a solid red sphere to the scene

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -4,6 +4,28 @@ mod sphere;
 // Exports.
 pub use sphere::Sphere;
 
+// Imports.
+use crate::types::Ray;
+
+/// An intersectable object.
+#[non_exhaustive]
+#[derive(Debug, Clone, Copy)]
+pub enum Object {
+    Sphere(Sphere),
+}
+
+/// Check whether a ray intersects an object.
+///
+/// # Arguments
+///
+/// * `ray` - the ray to trace along
+/// * `object` - the object to intersect
+pub fn intersects(ray: &Ray, object: &Object) -> bool {
+    match object {
+        Object::Sphere(s) => s.intersected_by(ray),
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
