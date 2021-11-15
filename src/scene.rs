urdh/raytrust

commit 749e15d324a357d99dd1f8bfd0701b278edcca7e

Use a separate Color type for materials

src/scene.rs
@@ -70,8 +70,8 @@ impl Scene {
             let acc = scatters
                 .iter()
                 .map(|(reflected, attenuation)| {
-                    let rendered = self.render_ray(reflected, depth - 1);
-                    rendered * attenuation
+                    self.render_ray(reflected, depth - 1)
+                        * image::Pixel(attenuation.red(), attenuation.green(), attenuation.blue())
                 })
                 .fold(image::Pixel::default(), |acc, pixel| acc + pixel);
             if !scatters.is_empty() {
@@ -95,7 +95,7 @@ mod test {
 
     #[test]
     fn test_intersection_filter() {
-        let material = Lambertian::new(1.0, 1.0, 1.0);
+        let material = Lambertian::new(Color(1.0, 1.0, 1.0));
         let sphere = Sphere {
             center: Point3(0.0, 0.0, 2.0),
             radius: 1.0,
@@ -115,7 +115,7 @@ mod test {
 
     #[test]
     fn test_multiple_objects() {
-        let material = Lambertian::new(1.0, 1.0, 1.0);
+        let material = Lambertian::new(Color(1.0, 1.0, 1.0));
         let sphere_a = Sphere {
             center: Point3(0.0, 0.0, 2.0),
             radius: 1.0,

src/lib.rs
@@ -10,7 +10,7 @@ mod types;
 
 use camera::Camera;
 pub use image::Image;
-use materials::{Hemispherical, Lambertian, Metal};
+use materials::{Color, Hemispherical, Lambertian, Metal};
 use scene::{Object, Scene};
 use surfaces::Sphere;
 use types::{Point3, Vect3};
@@ -25,7 +25,7 @@ pub fn get_scene() -> Scene {
                     center: Point3(-1.0, 0.0, -1.0),
                     radius: 0.5,
                 }),
-                material: Box::new(Metal::new(0.8, 0.8, 0.8, 0.3)),
+                material: Box::new(Metal::new(Color(0.8, 0.8, 0.8), 0.3)),
             },
             // Center diffuse sphere.
             Object {
@@ -33,7 +33,7 @@ pub fn get_scene() -> Scene {
                     center: Point3(0.0, 0.0, -1.0),
                     radius: 0.5,
                 }),
-                material: Box::new(Lambertian::new(0.7, 0.3, 0.3)),
+                material: Box::new(Lambertian::new(Color(0.7, 0.3, 0.3))),
             },
             // Right side metal sphere.
             Object {
@@ -41,7 +41,7 @@ pub fn get_scene() -> Scene {
                     center: Point3(1.0, 0.0, -1.0),
                     radius: 0.5,
                 }),
-                material: Box::new(Metal::new(0.8, 0.6, 0.2, 1.0)),
+                material: Box::new(Metal::new(Color(0.8, 0.6, 0.2), 1.0)),
             },
             // "Ground" sphere.
             Object {
@@ -49,7 +49,7 @@ pub fn get_scene() -> Scene {
                     center: Point3(0.0, -100.5, -1.0),
                     radius: 100.0,
                 }),
-                material: Box::new(Hemispherical::new(0.8, 0.8, 0.0)),
+                material: Box::new(Hemispherical::new(Color(0.8, 0.8, 0.0))),
             },
         ],
     }

src/materials/diffuse.rs
@@ -1,5 +1,4 @@
-use super::Material;
-use crate::image::Pixel;
+use super::{Color, Material};
 use crate::surfaces::Intersection;
 use crate::types::{Point3, Ray, Vect3};
 use rand::{thread_rng, Rng};
@@ -26,20 +25,18 @@ fn rand_point_on_sphere(origin: &Point3, radius: f32) -> Point3 {
 /// A lambertian diffuse material.
 #[derive(Debug, Clone, Copy)]
 pub struct Lambertian {
-    attenuation: Pixel,
+    attenuation: Color,
 }
 
 impl Lambertian {
     /// Construct a colored diffuse material with lambertian reflection.
-    pub fn new(r: f32, g: f32, b: f32) -> Lambertian {
-        Lambertian {
-            attenuation: Pixel(r, g, b),
-        }
+    pub fn new(color: Color) -> Lambertian {
+        Lambertian { attenuation: color }
     }
 }
 
 impl Material for Lambertian {
-    fn scatter_at(&self, _ray: &Ray, intersection: &Intersection) -> Vec<(Ray, Pixel)> {
+    fn scatter_at(&self, _ray: &Ray, intersection: &Intersection) -> Vec<(Ray, Color)> {
         let origin = intersection.point();
         let center = origin + intersection.normal();
         let direction = rand_point_on_sphere(&center, 1.0) - origin;
@@ -54,20 +51,18 @@ impl Material for Lambertian {
 /// A hemispherical diffuse material.
 #[derive(Debug, Clone, Copy)]
 pub struct Hemispherical {
-    attenuation: Pixel,
+    attenuation: Color,
 }
 
 impl Hemispherical {
     /// Construct a colored diffuse material with hemispherical reflection.
-    pub fn new(r: f32, g: f32, b: f32) -> Hemispherical {
-        Hemispherical {
-            attenuation: Pixel(r, g, b),
-        }
+    pub fn new(color: Color) -> Hemispherical {
+        Hemispherical { attenuation: color }
     }
 }
 
 impl Material for Hemispherical {
-    fn scatter_at(&self, _ray: &Ray, intersection: &Intersection) -> Vec<(Ray, Pixel)> {
+    fn scatter_at(&self, _ray: &Ray, intersection: &Intersection) -> Vec<(Ray, Color)> {
         let origin = intersection.point();
         let direction = rand_point_on_sphere(&origin, 1.0) - origin;
         if direction.dot(intersection.normal()) > 0.0 {
@@ -87,7 +82,7 @@ mod test {
     fn test_lambertian_reflects_outward() {
         let ray = Ray::new(Point3::zero(), Vect3(0.0, 0.0, 1.0));
         let intersection = Intersection::new(Point3::zero(), Vect3(0.0, 0.0, 1.0));
-        let lambertian = Lambertian::new(1.0, 1.0, 1.0);
+        let lambertian = Lambertian::new(Color(1.0, 1.0, 1.0));
         let scatters = lambertian.scatter_at(&ray, &intersection);
 
         for (reflection, _) in scatters {
@@ -100,7 +95,7 @@ mod test {
     fn test_hemispherical_reflects_outward() {
         let ray = Ray::new(Point3::zero(), Vect3(0.0, 0.0, 1.0));
         let intersection = Intersection::new(Point3::zero(), Vect3(0.0, 0.0, 1.0));
-        let hemispherical = Hemispherical::new(1.0, 1.0, 1.0);
+        let hemispherical = Hemispherical::new(Color(1.0, 1.0, 1.0));
         let scatters = hemispherical.scatter_at(&ray, &intersection);
 
         for (reflection, _) in scatters {

src/materials/mod.rs
@@ -7,10 +7,30 @@ pub use diffuse::{Hemispherical, Lambertian};
 pub use reflective::Metal;
 
 // Imports.
-use crate::image::Pixel;
 use crate::surfaces::Intersection;
 use crate::types::Ray;
 use std::vec::Vec;
+
+/// A color with red/green/blue components.
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
+pub struct Color(pub f32, pub f32, pub f32);
+
+impl Color {
+    /// The red channel of the color.
+    pub fn red(&self) -> f32 {
+        self.0
+    }
+
+    /// The green channel of the color.
+    pub fn green(&self) -> f32 {
+        self.1
+    }
+
+    /// The blue channel of the color.
+    pub fn blue(&self) -> f32 {
+        self.2
+    }
+}
 
 /// A (possibly reflecting) material.
 pub trait Material {
@@ -20,5 +40,5 @@ pub trait Material {
     ///
     /// * `ray` - ray to reflect
     /// * `intersection` - intersection to reflect at
-    fn scatter_at(&self, ray: &Ray, intersection: &Intersection) -> Vec<(Ray, Pixel)>;
+    fn scatter_at(&self, ray: &Ray, intersection: &Intersection) -> Vec<(Ray, Color)>;
 }

src/materials/reflective.rs
@@ -1,5 +1,4 @@
-use super::Material;
-use crate::image::Pixel;
+use super::{Color, Material};
 use crate::surfaces::Intersection;
 use crate::types::{Ray, Vect3};
 use rand::{thread_rng, Rng};
@@ -22,22 +21,22 @@ fn rand_point_on_disk(normal: &Vect3, radius: f32) -> Vect3 {
 /// A reflective metal-like material.
 #[derive(Debug, Clone, Copy)]
 pub struct Metal {
-    attenuation: Pixel,
+    attenuation: Color,
     pertubation: f32,
 }
 
 impl Metal {
     /// Construct a metal material with a given attenuation.
-    pub fn new(r: f32, g: f32, b: f32, fuzziness: f32) -> Metal {
+    pub fn new(color: Color, fuzziness: f32) -> Metal {
         Metal {
-            attenuation: Pixel(r, g, b),
+            attenuation: color,
             pertubation: fuzziness,
         }
     }
 }
 
 impl Material for Metal {
-    fn scatter_at(&self, ray: &Ray, intersection: &Intersection) -> Vec<(Ray, Pixel)> {
+    fn scatter_at(&self, ray: &Ray, intersection: &Intersection) -> Vec<(Ray, Color)> {
         let normal = intersection.normal();
         let incident = ray.direction();
         let reflection = incident - 2.0 * incident.dot(normal) * normal;

commit 41032e99fccd9dbadf6c0bf6c7d6aea9d605af24

Fix background broken by #28

src/scene.rs
@@ -81,7 +81,7 @@ impl Scene {
             }
         } else {
             // Fall-back: fancy blue-ish gradient
-            let t = 0.5 * (ray.direction().z() + 1.0);
+            let t = 0.5 * (ray.direction().y() + 1.0);
             ((1.0 - t) * image::Pixel(1.0, 1.0, 1.0)) + (t * image::Pixel(0.5, 0.7, 1.0))
         }
     }